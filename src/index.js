d7e58f56ad92a9fe65665168b56486da74bc911d
Patrick Selin <patrick.selin@gmail.com>
2023-06-16 10:53:56 +0300

    form and fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
-console.log("hi")
+const searchButton = document.getElementById('submit-data');
+const searchField = document.getElementById('input-show');
+
+
+
+function fetchQuery(searchQuery) {
+  fetch('https://api.tvmaze.com/search/shows?q='
+    + searchQuery)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let = queryData = data;
+      console.log(data);
+
+      for (let i = 0; i < queryData.length; i++) {
+        console.log(queryData[i]);
+        console.log(queryData[i].show.image.medium);
+        console.log(queryData[i].show.name);
+        console.log(queryData[i].show.summary);
+
+        const markup = `<div class="show-data"> 
+            <img src="${queryData[i].show.image.medium}"> 
+            <div class="show-info"> 
+                <h1>${queryData[i].show.name}</h1> 
+                ${queryData[i].show.summary}
+            </div> 
+        </div>`;
+
+  
+
+        document.querySelector(".data-container")
+        .insertAdjacentHTML('beforeend', markup);
+
+      }
+
+
+    });
+}
+
+
+searchButton.addEventListener("click", function (event) {
+  event.preventDefault()
+
+  const searchQuery = searchField.value;
+
+  fetchQuery(searchQuery);
+
+  // queryData.array.forEach(element => {
+  //   console.log(element)
+  // });
+
+
+
+});
+
